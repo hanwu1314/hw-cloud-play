06168e42979fb7f47ae819f67fa145679c894cc3
hanwu <hanwu1314@outlook.com>
2023-08-17 13:05:50 +0800

    登录注册重构优化

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -25,82 +25,130 @@ class Index extends Controller
     public function register()
     {
         // 判断是否已经登录 登录后重定向到首页
-        $LoginBusiness = cookie('LoginBusiness') ?? '';
-        if ($LoginBusiness) {
-            $this->redirect(url('/home/business/index'));
+        if ($this->isUserLoggedIn()) {
+            $this->redirectToHome();
         }
+
         if ($this->request->isPost()) {
             $mobile = $this->request->param('mobile', '', 'trim');
             $password = $this->request->param('password', '', 'trim');
 
-
-            if (empty($password)) {
-                $this->error('密码不能为空');
+            // 验证参数
+            if (empty($mobile) || empty($password)) {
+                $this->error('手机号和密码不能为空');
             }
-            /** 生成密码盐 */
+
+            // 生成密码盐和哈希密码
             $salt = build_ranstr();
-            /** md5密码 */
-            $password = md5($password . $salt);
+            $hashedPassword = password_hash($password . $salt, PASSWORD_DEFAULT);
 
             // 组装注册的数据
             $data = [
                 'mobile' => $mobile,
-                'password' => $password,
+                'password' => $hashedPassword,
                 'salt' => $salt,
                 'money' => 0,
                 'auth' => 0,
                 'deal' => 0,
             ];
 
-            // ? 查询用户来源
+            //  查询用户来源
             $source = model('business.Source')->where(['name' => ['LIKE', '%云课堂%']])->find();
             if ($source) {
                 $data['sourceid'] = $source['id'];
             }
 
-            // halt($data);
-            $result = $this->BusinessModel->validate('common/business/Business.register')->save($data);
+            // 保存用户数据
+            $result = $this->registerUser($data);
             if ($result === false) {
-                $this->error($this->BusinessModel->getError());
+                $this->error($this->getRegisterError());
             } else {
                 $this->success('注册成功', url('/home/index/login'));
             }
         }
+
         return $this->fetch();
     }
+
     /**
      * 登录
      */
     public function login()
     {
         // 判断是否已经登录 登录后重定向到首页
-        $LoginBusiness = cookie('LoginBusiness') ?? '';
-        if ($LoginBusiness) {
-            $this->redirect(url('/home/business/index'));
+        if ($this->isUserLoggedIn()) {
+            $this->redirectToHome();
         }
+
         if ($this->request->isPost()) {
             $mobile = $this->request->param('mobile', '', 'trim');
             $password = $this->request->param('password', '', 'trim');
 
-            $business = $this->BusinessModel->where(['mobile' => $mobile])->find();
+            // 查询用户
+            $user = $this->getUserByMobile($mobile);
 
-            if (!$business) {
+            if (!$user) {
                 $this->error('用户不存在');
             }
-            if (md5($password . $business['salt']) != $business['password']) {
+            // 验证密码
+            if (!password_verify($password . $user['salt'], $user['password'])) {
                 $this->error('密码错误');
             }
 
+            // 设置登录信息
             $data = [
-                'id' => $business['id'],
-                'mobile' => $business['mobile']
+                'id' => $user['id'],
+                'mobile' => $user['mobile']
             ];
             cookie('LoginBusiness', $data);
 
             $this->success('登录成功', url('home/business/index'));
         }
+
         return $this->fetch();
     }
+
+    /**
+     * 判断是否已经登录
+     */
+    private function isUserLoggedIn()
+    {
+        $LoginBusiness = cookie('LoginBusiness') ?? '';
+        return !empty($LoginBusiness);
+    }
+
+    /**
+     * 重定向到首页
+     */
+    private function redirectToHome()
+    {
+        $this->redirect(url('/home/business/index'));
+    }
+
+    /**
+     * 注册用户
+     */
+    private  function registerUser($data)
+    {
+        return $this->BusinessModel->validate('common/business/Business.register')->save($data);
+    }
+
+    /**
+     * 获取注册错误信息
+     */
+    private  function getRegisterError()
+    {
+        return $this->BusinessModel->getError();
+    }
+
+    /**
+     * 根据手机号获取用户
+     */
+    private function getUserByMobile($mobile)
+    {
+        return $this->BusinessModel->where(['mobile' => $mobile])->find();
+    }
+
     /**
      * 注销
      */
