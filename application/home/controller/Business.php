f8d4e41840392781a1380c764176435bf7af2e11
hanwu <hanwu1314@outlook.com>
2023-08-17 14:15:35 +0800

    修改资料页面

--- application/home/controller/Business.php
+++ application/home/controller/Business.php
@@ -6,7 +6,24 @@ use app\common\controller\Home;
 
 class Business extends Home
 {
+
+    /** 用户模型 */
+    protected $BusinessModel = null;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->BusinessModel = model('business.Business');
+    }
+
     public function index()
+    {
+        return $this->fetch();
+    }
+    /**
+     * 修改资料
+     */
+    public function profile()
     {
         return $this->fetch();
     }
